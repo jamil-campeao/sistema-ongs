b84a973c6a2bbcb1848cff7dbaa0c93e58ef9bc2
Jamil Campeão <jamilcampeao@gmail.com>
2025-10-10 21:34:44 -0300

    feat: Adicionado teste de header

--- frontend/src/app/api/email.js
+++ frontend/src/app/api/email.js
@@ -1,24 +1,25 @@
-import emailjs from '@emailjs/browser';
+import emailjs from "@emailjs/browser";
 
 emailjs.init({
-    publicKey: 'hhW_LhDgy1nw2Uhol'
+  publicKey: "hhW_LhDgy1nw2Uhol",
 });
 
 export async function envioEmail(email, assunto, mensagem) {
-    try {
-        const response = await emailjs.send('service_e0vv17c', 'template_7jfrirb', {
-            assunto: assunto,
-            mensagem: mensagem,
-            email: email,
-        });
-        return response;
-    }
-    catch (error) {
-        throw new Error('Erro ao enviar o email:');
-    }
+  try {
+    const response = await emailjs.send("service_e0vv17c", "template_7jfrirb", {
+      assunto: assunto,
+      mensagem: mensagem,
+      email: email,
+    });
+    return response;
+  } catch (error) {
+    throw new Error("Erro ao enviar o email:");
+  }
 }
 
 export function geraCodigoEmail() {
-    const codigo = Math.floor(Math.random() * 1000000).toString().padStart(6, '0');
-    return codigo;
+  const codigo = Math.floor(Math.random() * 1000000)
+    .toString()
+    .padStart(6, "0");
+  return codigo;
 }
