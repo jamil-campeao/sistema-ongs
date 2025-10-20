bb8f32ebc9222db1b796ced631a924d1102373cd
Jamil Campeão <jamilcampeao@gmail.com>
2025-10-20 17:52:54 -0300

    feat: Integrado Prometheus e Grafana para monitoramento; atualiza Dockerfile e configurações

--- backend/src/app.js
+++ backend/src/app.js
@@ -4,6 +4,10 @@ import cors from "cors";
 const app = express();
 import "./instrument.js";
 import * as Sentry from "@sentry/node"
+import client from 'prom-client';
+
+const collectDefaultMetrics = client.collectDefaultMetrics;
+collectDefaultMetrics();
 
 app.use(express.json({ limit: "1000mb" }));
 app.use(
@@ -14,6 +18,16 @@ app.use(
   })
 );
 app.use(routes);
+
+app.get('/metrics', async (req, res) => {
+  try {
+    res.set('Content-Type', client.register.contentType);
+    res.end(await client.register.metrics());
+  } catch (ex) {
+    res.status(500).end(ex);
+  }
+});
+
 Sentry.setupExpressErrorHandler(app);
 
 export default app;
